775778f2a7549b4051d2ed13febff0e51b33b487
Smok123780 <smok123780@gmail.com>
2019-09-20 15:01:44 +0200

    Adding company table with reference to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_20_125906) do
+ActiveRecord::Schema.define(version: 2019_09_20_130031) do
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_companies_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
@@ -22,4 +30,5 @@ ActiveRecord::Schema.define(version: 2019_09_20_125906) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "companies", "users"
 end
